67f943455257480274afbb49dc18495487629539
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-11-08 14:18:26 -0500

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,111 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  mounted() {
+    $("#items").sortable({
+      handle: ".dragger-item",
+    });
+    $(".boton").click(function () {
+      $(this).toggleClass("inactivo activo");
+      $(this).find(".fa-solid").toggleClass(".fa-thumbs-down fa-thumbs-up")
+    });
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- <ul id="items">
+    <li class="flex gap-4 items-center" v-for="i in 5">
+      <span class="font-bold text-2xl dragger">X</span>
+      <span class="font-bold text-2xl">Bendiciones</span>
+    </li>
+  </ul> -->
+
+  <div class="bg-black/60 h-screen flex items-center justify-center">
+    <div class="bg-white rounded-lg shadow w-full max-w-2xl p-4 grid gap-4">
+      <h1 class="font-bold text-xl">Limpieza de departamentos</h1>
+      <div class="overflow-x-auto">
+        <table class="w-full">
+          <thead>
+            <tr class="bg-blue-900 rounded text-white text-xs">
+              <th class="py-6"></th>
+              <th>Orden</th>
+              <th class="text-left">Departamento</th>
+              <th>Sala</th>
+              <th>Cocina</th>
+              <th>Habitaciones</th>
+              <th>SSHH</th>
+              <th>Completado</th>
+            </tr>
+          </thead>
+          <tbody id="items">
+            <tr class="item bg-gray-100" v-for="i in 10">
+              <td class="pl-4 py-2">
+                <button class="dragger-item">
+                  <i class="fa-solid fa-grip-vertical"> </i>
+                </button>
+              </td>
+              <td class="text-xs font-bold text-center">{{ i }}</td>
+              <td class="text-xs font-bold">A301</td>
+              <td>
+                <div class="w-full flex justify-center text-xs">
+                  <button
+                    class="rounded-full w-6 h-6 boton inactivo flex items-center justify-center"
+                  >
+                    <i class="fa-solid fa-thumbs-down"></i>
+                  </button>
+                </div>
+              </td>
+
+              <td>
+                <div class="w-full flex justify-center text-xs">
+                  <button
+                    class="rounded-full w-6 h-6 boton inactivo flex items-center justify-center"
+                  >
+                    <i class="fa-solid fa-thumbs-down"></i>
+                  </button>
+                </div>
+              </td>
+              <td>
+                <div class="w-full flex justify-center text-xs">
+                  <button
+                    class="rounded-full w-6 h-6 boton inactivo flex items-center justify-center"
+                  >
+                    <i class="fa-solid fa-thumbs-down"></i>
+                  </button>
+                </div>
+              </td>
+
+              <td>
+                <div class="w-full flex justify-center text-xs">
+                  <button
+                    class="rounded-full w-6 h-6 boton inactivo flex items-center justify-center"
+                  >
+                    <i class="fa-solid fa-thumbs-down"></i>
+                  </button>
+                </div>
+              </td>
+              <td class="bg-gray-200">
+                <div class="w-full flex justify-center">
+                  <button
+                    class="rounded-full w-6 h-6 boton inactivo flex items-center justify-center"
+                  >
+                    <i class="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.inactivo {
+  @apply bg-red-200 text-red-600;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.activo {
+  @apply bg-green-200 text-green-600;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["'Inter'", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
